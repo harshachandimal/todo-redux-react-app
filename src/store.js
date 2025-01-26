4e118f54137b404e688bd2255c750ba70aab02fe
Harsha <harshachandim92@gmail.com>
2025-01-26 23:18:24 +0530

    Config redux store persist.

--- src/store.js
+++ src/store.js
@@ -1,12 +1,19 @@
-import {configureStore} from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import tasksReducer from "./utilities/taskSlice.js";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
 
-export default configureStore({
-reducer: {
-    tasks : tasksReducer,
-
-
-
+const persistConfig = {
+    key: 'root',
+    storage
 }
 
+export const persistedReducer = persistReducer(persistConfig, tasksReducer)
+export const store = configureStore({
+  reducer: {
+    tasks: persistedReducer,
+  }
 })
+
+export const persistor = persistStore(store);
+
